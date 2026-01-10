287742f430f993867de8c03bd629de11e4c83a30
MrNeutron199 <rspandit468@gmail.com>
2026-01-10 19:27:13 +0530

    Added a error handler and a logging message

--- server.mjs
+++ server.mjs
@@ -6,28 +6,44 @@ const app = express();
 
 function downloadVideoAsBlob(videoUrl, mimeType) {
   return new Promise((resolve, reject) => {
-    const outputBase = `temp_audio_${Date.now()}`;
-    const finalAudioFile = `${outputBase}.m4a`;
-    const params = `-f bestaudio[ext=m4a]/91/92 --embed-thumbnail -x --audio-format m4a --audio-quality 0 --no-keep-video --js-runtimes node -o ${outputBase} ${videoUrl}`;
-    const ytdlpProcess = spawn("yt-dlp", params.split(" "));
-    ytdlpProcess.on("close", (code) => {
-      if (code !== 0) {
-        reject(new Error(`yt-dlp exited with code ${code}`));
-        return;
-      }
-      const audioData = fs.readFileSync(finalAudioFile);
-      const videoBlob = new Blob([audioData], { type: mimeType });
-      fs.unlinkSync(finalAudioFile);
-      resolve(videoBlob);
-    });
-    ytdlpProcess.on("error", (err) => {
-      reject(err);
-    });
+    let attempts = 0;
+    const maxRetries = 3;
+    const attempt = () => {
+      const outputBase = `temp_audio_${Date.now()}`;
+      const finalAudioFile = `${outputBase}.m4a`;
+      const params = `-f bestaudio[ext=m4a]/91/92 --embed-thumbnail -x --audio-format m4a --audio-quality 0 --no-keep-video --js-runtimes node -o ${outputBase} ${videoUrl}`;
+      const ytdlpProcess = spawn("yt-dlp", params.split(" "));
+      ytdlpProcess.on("close", (code) => {
+        if (code === 0) {
+          const audioData = fs.readFileSync(finalAudioFile);
+          const videoBlob = new Blob([audioData], { type: mimeType });
+          fs.unlinkSync(finalAudioFile);
+          resolve(videoBlob);
+        } else {
+          handleFailure(`Process exited with code ${code}`);
+        }
+      });
+      ytdlpProcess.on("error", (err) => {
+        handleFailure(err);
+      });
+      const handleFailure = (err) => {
+        if (attempts < maxRetries) {
+          attempts++;
+          console.log(`Attempt ${retryCount} failed. Retrying...`);
+          // attempt();
+          setTimeout(() => attempt(), 30 * 1000);
+        } else {
+          reject(new Error("Max retries reached: " + err.message));
+        }
+      };
+    };
+    attempt();
   });
 }
 
 app.get("/play", (req, res) => {
   const url = req.headers["url"];
+  console.log(`Received a request to download ${url}`);
   downloadVideoAsBlob(url, "audio/m4a").then((blob) => {
     const str = Readable.fromWeb(blob.stream());
     if (str) {
